1a1cffa8b26ab7e25b1db794094e32834cc3d41c
sheshum <36011215+sheshum@users.noreply.github.com>
2018-02-07 11:49:17 +0100

    navbar, background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App-body">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <nav id="nav-wrap">
+            <ul id="nav">
+              <li><a>Naslovna</a></li>
+              <li><a>Repertoar</a></li>
+              <li><a>Cenovnik</a></li>
+              <li><a>O nama</a></li>
+              <li><a>Kontakt</a></li>
+            </ul>
+
+          </nav>
+         
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
