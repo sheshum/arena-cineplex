1e5ade9fa57bfc98b219d10c1badac07cd5d7ac7
sheshum <36011215+sheshum@users.noreply.github.com>
2018-02-09 00:24:48 +0100

    header.css transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,8 +12,8 @@ class Header extends Component{
             <div id="nav-wrapper">
                 <nav id="nav-wrap">
                     <ul id="nav">
-                      <li><NavLink to="/home" activeStyle={{color: "#F06000"}} >Naslovna</NavLink></li>
-                      <li><NavLink to="/repertoar" activeStyle={{color: "#F06000"}} >Repertoar</NavLink></li>
+                      <li><NavLink to="/home" >Naslovna</NavLink></li>
+                      <li><NavLink to="/repertoar"  >Repertoar</NavLink></li>
                       <li><a>Cenovnik</a></li>
                       <li>
                           <a className="dropdown">O nama</a>
